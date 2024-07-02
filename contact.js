7cd64f503a397cc77e3a20dd4313e75c35ac2a21
aikajackson <aikaljackson@gmail.com>
2024-07-02 17:52:18 -0500

    subfinal edits

--- contact.js
+++ contact.js
@@ -27,3 +27,57 @@ toggleButtonTwo.addEventListener("click", function(){
 toggleButtonThree.addEventListener("click", function(){
     aboutChallenge.classList.toggle("active");
 })
+
+let appear = document.querySelector(".appear"); 
+let cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("inview");
+    }else{
+      entry.target.classList.remove("inview");
+    }
+  });
+}
+let io = new IntersectionObserver(cb);
+io.observe(appear)
+let items = document.querySelectorAll(".appear2");
+let active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add("inview2"); 
+        }else{
+            entry.target.classList.remove("inview2"); 
+        }
+    });
+}
+let io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
+
+ let appearThree = document.querySelector(".appear3"); 
+ let cb2 = function(entries){
+   entries.forEach(entry => {
+     if(entry.isIntersecting){
+       entry.target.classList.add("inview3");
+     }else{
+       entry.target.classList.remove("inview3");
+     }
+   });
+ }
+ let io3 = new IntersectionObserver(cb2);
+ io3.observe(appearThree)
+ let itemsTwo = document.querySelectorAll(".appear4");
+ let activeTwo = function(entries){
+     entries.forEach(entry => {
+         if(entry.isIntersecting){
+         entry.target.classList.add("inview4"); 
+         }else{
+             entry.target.classList.remove("inview4"); 
+         }
+     });
+ }
+ let io4 = new IntersectionObserver(activeTwo);
+  for(let i=0; i < items.length; i++){
+     io4.observe(items[i]);
+  }
--- home.js
+++ home.js
@@ -12,7 +12,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -21,3 +20,57 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
+let appear = document.querySelector(".appear"); 
+let cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("inview");
+    }else{
+      entry.target.classList.remove("inview");
+    }
+  });
+}
+let io = new IntersectionObserver(cb);
+io.observe(appear)
+let items = document.querySelectorAll(".appear2");
+let active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add("inview2"); 
+        }else{
+            entry.target.classList.remove("inview2"); 
+        }
+    });
+}
+let io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
+ 
+ 
+ let appearThree = document.querySelector(".appear3"); 
+ let cb2 = function(entries){
+   entries.forEach(entry => {
+     if(entry.isIntersecting){
+       entry.target.classList.add("inview3");
+     }else{
+       entry.target.classList.remove("inview3");
+     }
+   });
+ }
+ let io3 = new IntersectionObserver(cb2);
+ io3.observe(appearThree)
+ let itemsTwo = document.querySelectorAll(".appear4");
+ let activeTwo = function(entries){
+     entries.forEach(entry => {
+         if(entry.isIntersecting){
+         entry.target.classList.add("inview4"); 
+         }else{
+             entry.target.classList.remove("inview4"); 
+         }
+     });
+ }
+ let io4 = new IntersectionObserver(activeTwo);
+  for(let i=0; i < items.length; i++){
+     io4.observe(items[i]);
+  }
