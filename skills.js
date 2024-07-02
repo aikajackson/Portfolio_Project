00c9fafa25132fd44c3a9c18d95c4b39c52feb57
aikajackson <aikaljackson@gmail.com>
2024-07-02 18:02:22 -0500

    -m "6pm updates"

--- skills.js
+++ skills.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,27 +12,48 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
-//function showSlides() {
-//    let i;
-//    let slides = document.getElementsByClassName("mySlides");
-//    for (i = 0; i < slides.length; i++) {
-//      slides[i].style.display = "none";
-//    }
-//    slideIndex++;
-//    if (slideIndex > slides.length) {slideIndex = 1}
-//    slides[slideIndex-1].style.display = "block";
-//    setTimeout(showSlides, 2000);
-//  }
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+let appear = document.querySelector(".appear"); 
+let cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("inview");
+    }else{
+      entry.target.classList.remove("inview");
+    }
+  });
+}
+let io = new IntersectionObserver(cb);
+io.observe(appear)
+let items = document.querySelectorAll(".appear2");
+let active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add("inview2"); 
+        }else{
+            entry.target.classList.remove("inview2"); 
+        }
+    });
+}
+let io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
